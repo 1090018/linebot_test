836b895eb5182e156ce47a77ab01f943cf4a87f2
租圈 <135095541+1090018@users.noreply.github.com>
2023-11-20 22:28:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from linebot.models import *
 #======這裡是呼叫的檔案內容=====
 from message import *
 from new import *
-from Function import *
+from function import *
 from mongodb_function import *
 #======這裡是呼叫的檔案內容=====
 
